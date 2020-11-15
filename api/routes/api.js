c89cdc6cf7bf8530cc17ba807422421e0b9ad31d
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-11-15 13:15:34 -0300

    rota tabela de precos

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,11 +2,13 @@ var express = require("express");
 var authRouter = require("./auth");
 var bookRouter = require("./book");
 var dicasRouter = require("./dicas");
+const { tabelaPrecosList } = require("../globals/Globals");
 
 var app = express();
 
 app.use("/auth/", authRouter);
 app.use("/book/", bookRouter);
 app.use("/dicas/", dicasRouter);
+app.use("/tabela_precos", (req, res) => res.send({ tabela: tabelaPrecosList }));
 
 module.exports = app;
